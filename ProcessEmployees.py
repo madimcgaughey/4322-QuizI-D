1d9b5a63d602dbd97d936d3572a01ce1e2447667
madimcgaughey <madi_mcgaughey1@baylor.edu>
2023-02-13 15:13:16 -0600

    hello

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,19 +11,33 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
-
-
-
+employee_file = open('employee_data.csv', 'r')
 
 #create an empty dictionary
+employee_dict = {}
+list1 = employee_dict['employee']
 
 
 #use a loop to iterate through the csv file
 
+        #employee_dict['Name'] = firstname, lastname
+        #employee_dict['Salary'] = newsalary
+        #print(employee_dict)
 
-    #check if the employee fits the search criteria
+
+for employee in employee_file:
+  fn = employee['First Name']
+  ln = employee['Last Name']
+  salary = employee['Salary']
+  newsalary = salary*(salary*.1)
+  if newsalary != 0:
+    employee_dict['Name'] = fn, ln
+    employee_dict['New Salary'] = newsalary
+    print(employee_dict)
 
 
+
+  
     
 
 print()
